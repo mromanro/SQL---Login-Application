d67b15962fa8db3a833861d482ea55854bdd8807
Miguel Roman <mromanro117@gmail.com>
2014-11-20 10:13:56 -0700

    made a few changes to DocumentController

--- src/javafxapplication4/FXMLDocumentController.java
+++ src/javafxapplication4/FXMLDocumentController.java
@@ -13,6 +13,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -35,23 +38,50 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         if(event.getSource() == submitButton) {
-            String username = usernameField.getText();
-            String password = passwordField.getText();
-            
-            database.validateLogin(username, password);
-            setErrorMssg("Submit button clicked");
+            attemptLogin();
         }
-        
+    }
+    
+    @FXML
+    private void handleKeyPressed(KeyEvent event) {
+        if(event.getCode() == KeyCode.ENTER){
+            if(passwordField.isFocused()){
+                setMssg("Attempting Log In", false);
+                attemptLogin();
+            }
+            else{
+                passwordField.requestFocus();
+                setMssg("pressed enter", false);
+            }
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         mssgLabel.setVisible(false);
-        
         database = new OracleJdbc();
     }    
     
-    public void setErrorMssg(String mssg) {
+    public void attemptLogin(){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        
+        if(username.isEmpty())
+            setMssg("Type in Username", true);
+        else if(password.isEmpty())
+            setMssg("Type in Password", true);
+        else
+            database.validateLogin(username, password);
+    }
+    
+    public void setMssg(String mssg , boolean error) {
+        if(error){
+            mssgLabel.setTextFill(Color.RED);
+            
+        } else {
+            mssgLabel.setTextFill(Color.BLUE);
+        }
+        
         mssgLabel.setText(mssg);
         mssgLabel.setVisible(true);
     }
